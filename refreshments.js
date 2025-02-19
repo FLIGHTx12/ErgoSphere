a580e0adacd1e7084355c80a2755fc736fc299f0
FLIGHTx12 <Vcdillard@gmail.com>
2025-02-19 11:03:35 -0600

    refactor refreshments.js to dynamically load options from server and simplify container mapping

--- refreshments.js
+++ refreshments.js
@@ -1,58 +1,46 @@
-// Moved to global scope:
-const saltySnacks = [
-   "🌿Roasted Seaweed - 10💷", "🔺Tortilla Chips (Donkey/El Milagro) - 10💷", "🍿Popcorn (Boom chicka/Skinny pop) - 20💷", "🧀Cheez-it - 30💷", "🧀Simply Cheetos Puffs White Cheddar - 30💷",  
-   "🐄 Old Fashioned Beef Jerk - 40💷", "🍘Wheat Thins Original - 40💷"
-];
-const sweetSnacks = [
-  "🐮Chobani Whole Milk Plain Greek Yogurt - 10💷", "🍪Simple Truth Blueberry Breakfast Cookies - 20💷",  "🐻Chocolate Teddy Graham Snacks - 30💷", "🍫Dark Chocolate Covered Almonds/Raisins - 30💷", "🍪Belvita Blueberry Breakfast biscuits - 40💷",
-  "🍪Chips Ahoy 2 pack - 40💷", "🎂Little Bites (Fudge/Banana) - 40💷",
-];
-const frozenSnacks = [
-  "🍕Jacks Pizza Bois - 20💷", "🍨Breyers Mango Ice cream - 20💷", "🍕Totinos Pizza rolls - 30💷", "🥟Bibigo Chicken & Veggie Mini Wontos - 30💷", "🍨Kroger Deluxe artisan Vanilla bean ice cream - 40💷", 
-  "🍦So Delicious Vanilla Bean Coconut milk IceCream Sandwiches - 40💷",
-   
-];
-const badSnacks = [
-   
-     "🚬THC Gummies - 40💷", 
-];
-const drinks = [
- "☕VJ Hot Cocoa 500ml - 20💷", "🍺Beer - 50💷", "🥃Mixed Drink (2shots) - 50💷", "🍷Wine Glass 500ml - 100💷", "🍾Wine Bottle 750ml - 150💷"
-];
-// Merge badSnacks and drinks into one "concoctions" array
-const concoctions = badSnacks.concat(drinks);
-const mealMods = [
-  "🍔Fast Food Cheat Meal - 60💷", "🍴Lunch Snack - 2💷", "🌞Weekend AM Snack - 2💷" ,
-   "🎉SNACK-A-THON MOD(1/2) - 0💷", "🎉🎉SNACK-A-THON MOD(2/2) - 0💷"
-];
+// Remove static arrays and static optionsMap declarations
 
-const optionsMap = {
-  "saltySnackContainer": saltySnacks,
-  "sweetSnackContainer": sweetSnacks,
-  "frozenSnackContainer": frozenSnacks,
-  "concoctionsContainer": concoctions,
-  "mealModsContainer": mealMods
+// Mapping between container element IDs and keys from refreshment.json
+const containerMapping = {
+  saltySnackContainer: 'saltySnacks',
+  sweetSnackContainer: 'sweetSnacks',
+  frozenSnackContainer: 'frozenSnacks',
+  concoctionsContainer: 'concoctions',
+  mealModsContainer: 'mealMods'
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Refreshment containers and data arrays
-  const selectContainers = [
-    "saltySnackContainer", "sweetSnackContainer", "frozenSnackContainer", "concoctionsContainer",
-    "mealModsContainer"
-  ];
+let optionsMap = {};
 
-  // Populate selects and add "+" button listeners
-  populateSelectOptions();
-  selectContainers.forEach(selectContainerId => {
-    const selectContainer = document.getElementById(selectContainerId);
-    if (selectContainer) {
-      const addButton = selectContainer.querySelector('.add-button');
-      if (addButton) {
-        addButton.addEventListener('click', () => {
-          addNewSelection(selectContainerId);
-        });
+// Fetch options from the server and map them for each container
+function loadOptionsMap() {
+  return fetch('/api/refreshments')
+    .then(res => res.json())
+    .then(data => {
+      optionsMap = {};
+      Object.entries(containerMapping).forEach(([containerId, dataKey]) => {
+        optionsMap[containerId] = data[dataKey] || [];
+      });
+    })
+    .catch(err => console.error('Error loading refreshment options:', err));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadOptionsMap().then(() => {
+    // Refreshment containers and data arrays
+    const selectContainers = Object.keys(optionsMap);
+    // Populate selects and add "+" button listeners
+    populateSelectOptions();
+    selectContainers.forEach(selectContainerId => {
+      const selectContainer = document.getElementById(selectContainerId);
+      if (selectContainer) {
+        const addButton = selectContainer.querySelector('.add-button');
+        if (addButton) {
+          addButton.addEventListener('click', () => {
+            addNewSelection(selectContainerId);
+          });
+        }
       }
-    }
+    });
   });
 });
 
